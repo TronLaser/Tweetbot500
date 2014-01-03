84ed569a9a18484f3bab937cafb74d705ab8971d
TronLaser <systemneon460@gmail.com>
2014-01-03 06:55:04 +0400

    - Added debug mode that will tweet upon loading (togglable in config.ini) - Added code that makes sure the same text isn't tweeted twice in a row (this would make the API angry)

--- Tweetbot500.py
+++ Tweetbot500.py
@@ -17,6 +17,7 @@ consumer_key = config.get('login', 'consumer_key')
 consumer_secret = config.get('login', 'consumer_secret')
 access_token_key = config.get('login', 'access_token_key')
 access_token_secret = config.get('login', 'access_token_secret')
+debug = config.get('debug', 'debug')
 
 print ("Config file found and read")
 print ("Connecting to the API")
@@ -34,22 +35,43 @@ print ("READY!")
 # Below is the data for my tweetbot
 # Erase the stuff below and make what you want
 
+oldtweet = 'a' # I'm probably doing this part horribly wrong
+oldtweet = 'b' # If anyone wants to point out something wrong please do
+
 listoftweets = ['No', 'Nope', 'Negative', 'Nah', 'Not yet', 'Still no', 'Nyet', 'Nein', 'Nerp'] # The list of possible tweets
 
-def post(): # This will pick a message at random and tweet it
+def choose(): # This makes sure we don't tweet the same thing twice. If we do the API will get angry at us. 
+    global oldtweet
+    global tweet
     tweet = random.choice(listoftweets)
+    if oldtweet == tweet:
+        print ("Same tweet as last. Reselecting.")
+        choose()
+    else:
+        oldtweet = tweet
+        post()
+
+def post(): # This will post the message to Twitter
+    global tweet
     status = api.PostUpdate(tweet)
     print status.text
+
+# If we want to test the program without waiting, setting debug to 1 in the config will cause it to tweet upon loading
+if debug == ("1"):
+    print ("Debug ENABLED!")
+    choose()
+else:
+    print ("Debug disabled")
 
 while True: # This is the main loop for the program
     ctime = strftime("%M", gmtime())
     if ctime == "15":
-        post()
+        choose()
     elif ctime == "30":
-        post()
+        choose()
     elif ctime == "45":
-        post()
+        choose()
     elif ctime == "00":
-        post()
+        choose()
     time.sleep(60)
 
